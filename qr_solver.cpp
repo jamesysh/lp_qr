4280d0028df5f7fd7b252988200a365ea2a503a5
James <shaohua.yuan@stonybrook.edu>
2018-09-24 11:07:20 -0400

    try

--- qr_solver.cpp
+++ qr_solver.cpp
@@ -14,9 +14,13 @@ using namespace std;
 
 
 int main(){
+ 
+magma_int_t a =  magma_init();
+cout<<a<<endl;
     double A[12] = {1,2,3,4,5,6,7,6,5,4,3,2};
     //matrix initializing
-    double *B;
+    double *B = new double[12];
+    double *C = new double[12];
     int m = 4;
     int n = 3;
     int lda = 4;
@@ -27,9 +31,9 @@ int main(){
     //set up magma queue
     magma_queue_create(device, &queue_setmatrix);
     magma_setmatrix(m,n,sizeof(double),A,lda,B,lddb,queue_setmatrix); 
-    
-    
-   
+    magma_getmatrix(m,n,sizeof(double),B,lddb,C,lda,queue_setmatrix);
+    cout<<C[2]<<endl;
+    magma_finalize();
 
     return 0;
 
